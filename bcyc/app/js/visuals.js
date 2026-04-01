ae4ab89ccffecc168e39cfd04c49e116a5356f31
oyvindblikstad <brusmusic@gmail.com>
2026-04-01 11:42:42 +0200

    Update visuals.js

--- bcyc/app/js/visuals.js
+++ bcyc/app/js/visuals.js
@@ -185,7 +185,7 @@
         const baseAngle = Math.atan2(dy, dx);
         const angle = baseAngle * 0.18 + (Math.random() - 0.5) * 0.18;
 
-        const force = (1.8 + Math.random() * 2.0) * Math.sin(localP * Math.PI);
+        const force = (1.8 + Math.random() * 0.02) * Math.sin(localP * Math.PI);
 
         const pullFromCenter = (state.cx - p.x) * 0.03;
         p.vx += pullFromCenter;
